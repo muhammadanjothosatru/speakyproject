dfcd09c677793b504d2336358b277fd8c90bc385
silmihytn <silmihayatan1803126@students.um.ac.id>
2021-05-23 23:15:51 +0700

    added login layout

--- speaky-android/app/src/main/java/com/bighero/speaky/ui/splash/SplashActivity.kt
+++ speaky-android/app/src/main/java/com/bighero/speaky/ui/splash/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.bighero.speaky.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bighero.speaky.R
+import com.bighero.speaky.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
