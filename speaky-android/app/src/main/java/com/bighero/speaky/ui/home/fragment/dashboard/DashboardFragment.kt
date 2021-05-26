9cbcc1c4ba265732be2eef4013b54c26b8ed2551
silmihytn <silmihayatan1803126@students.um.ac.id>
2021-05-26 17:41:07 +0700

    added bottom navigation

--- speaky-android/app/src/main/java/com/bighero/speaky/ui/home/fragment/dashboard/DashboardFragment.kt
+++ speaky-android/app/src/main/java/com/bighero/speaky/ui/home/fragment/dashboard/DashboardFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.bighero.speaky.ui.home.databinding.FragmentDashboardBinding
+import com.bighero.speaky.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
