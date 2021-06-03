387ef63ae6ce6a496b40c854a99c6d674195fcb1
silmihytn <silmihayatan1803126@students.um.ac.id>
2021-06-03 15:58:33 +0700

    fix auth

---
+++ speaky-android/app/src/main/java/com/bighero/speaky/ui/home/fragment/module/ModuleAdapter.kt
@@ -0,0 +1,4 @@
+package com.bighero.speaky.ui.home.fragment.module
+
+class ModuleAdapter {
+}
--- speaky-android/app/src/main/java/com/bighero/speaky/ui/home/fragment/profile/ProfileFragment.kt
+++ speaky-android/app/src/main/java/com/bighero/speaky/ui/home/fragment/profile/ProfileFragment.kt
@@ -3,48 +3,101 @@ package com.bighero.speaky.ui.home.fragment.profile
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.bighero.speaky.R
+import com.bighero.speaky.databinding.ContentDetailProfileBinding
 import com.bighero.speaky.databinding.FragmentProfileBinding
 import com.bighero.speaky.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private lateinit var auth: FirebaseAuth
     private val binding get() = _binding!!
+    private lateinit var detailBinding: ContentDetailProfileBinding
+
+    private lateinit var database: DatabaseReference
+    private lateinit var uId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        detailBinding = binding.detailContent
         val root: View = binding.root
+        database = Firebase.database.reference
+        auth = Firebase.auth
+        uId = auth.currentUser!!.uid
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth = Firebase.auth
-        /*
-        binding.btLogout.setOnClickListener {
-            Firebase.auth.signOut()
-            startActivity((Intent(requireActivity(), LoginActivity::class.java)))
-            activity?.finish()
+        showLoading(true)
+        database.child("users").child(uId).child("name").get().addOnSuccessListener {
+            Log.i("firebase", "Got value ${it.value}")
+            detailBinding.profileName.text = it.value.toString()
+            showLoading(false)
+        }.addOnFailureListener {
+            Log.e("firebase", "Error getting data", it)
+            showLoading(false)
         }
+    }
 
-         */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.profile_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                Firebase.auth.signOut()
+                startActivity((Intent(requireActivity(), LoginActivity::class.java)))
+                activity?.finish()
+                true
+            }
+            R.id.action_delete -> {
+                showLoading(true)
+                //database.child("users").child(uId).removeValue()
+                auth.currentUser!!.delete()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("auth", "User account deleted.")
+                        }
+                    }
+                startActivity(Intent(activity, LoginActivity::class.java))
+                showLoading(false)
+                activity?.finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showLoading(i: Boolean) {
+        if (i) {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.content.visibility = View.INVISIBLE
+        } else {
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.content.visibility = View.VISIBLE
+        }
     }
 }
